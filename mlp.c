acf72d0266fb3e1c4fb63706156e20e6a9cd8063
jmaczan <jedrzejpawel@maczan.pl>
2025-05-23 22:19:12 +0200

    Intro to parameters initialization - biases are initialized with calloc because I want to make them all zeros at the beginning. Weights will be initialized with He since I use ReLU - I'm still reading about how to He, for now I understand that this is just sampling from a gaussian distribution with variance 2/n and standard deviation square root of 2/n where n is number of neurons in a given layer

--- mlp.c
+++ mlp.c
@@ -7,11 +7,11 @@
 // MNIST
 // MLP 784 (28*28 black/white pixels) -> 128 (hidden layer) -> 10 (digits)
 
-#define LINE_BUF_SIZE 4096
-#define INPUT_DIM 784
+#define LINE_BUF_SIZE 3137 // 1+784 * 4
+#define INPUT_DIM 784      // 28 x 28
 #define HIDDEN_LAYER_DIM 128
 #define OUTPUT_DIM 10
-#define MAX_TRAINING_EXAMPLES 3
+#define MAX_TRAINING_EXAMPLES 3 // * 1000
 
 #define RELU(i) (i > 0 ? i : 0)
 
@@ -23,35 +23,49 @@ void skip_csv_headers(FILE *f) {
     ;
 }
 
+// He initialization
+void initialize_weights(float *weights, int weights_length) {
+  // float scale_factor = 
+}
+
+// memory:
+// stack (up to 8MB)
+// [] - [] - [skip_csv_headers] - [main]
+
+// heap
+//
+
 int main() {
   char line[LINE_BUF_SIZE]; // on Stack
   FILE *f = fopen("data/mnist_train.csv", "r");
 
   skip_csv_headers(f);
 
-  float *weights_1 = malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float)); // on Heap
+  float *weights_1 =
+      malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float)); // on Heap
   float *weights_2 = malloc(HIDDEN_LAYER_DIM * OUTPUT_DIM * sizeof(float));
 
-  float *biases_1 = malloc(HIDDEN_LAYER_DIM * sizeof(float));
-  float *biases_2 = malloc(OUTPUT_DIM * sizeof(float));
+  float *biases_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
+  float *biases_2 = calloc(OUTPUT_DIM, sizeof(float));
 
-  // TODO: initialize weights and biases, ideally with some reasonable thing like Xavier
+  initialize_weights(weights_1, INPUT_DIM * HIDDEN_LAYER_DIM);
+  initialize_weights(weights_2, HIDDEN_LAYER_DIM * OUTPUT_DIM);
 
-  int remaining_training_examples = MAX_TRAINING_EXAMPLES;
-  while (remaining_training_examples > 0) {
+  int samples = MAX_TRAINING_EXAMPLES;
+  while (samples > 0) {
     fgets(line, LINE_BUF_SIZE, f);
-    char *token = strtok(line, ",\n");
+    char *token = strtok(line, ",\n"); // TODO: why ",\n" instead of "\n"?
 
     char label = token[0];
-    float input[INPUT_DIM];
+    float inputs[INPUT_DIM];
 
     // printf("%c \n", label);
     int i = 0;
     while (i < INPUT_DIM) {
       //   printf("%s \n", token);
       token = strtok(NULL, ",\n");
-      input[i] = atoi(token) / 255.0f; // a room for performance improvements
-                                       //   printf("%f \n", input[i]);
+      inputs[i] = atoi(token) / 255.0f; // a room for performance improvements
+                                        //   printf("%f \n", input[i]);
       i++;
     }
 
@@ -64,7 +78,7 @@ int main() {
     // TODO: loss and gradient descent
 
     // printf("%s", line);
-    remaining_training_examples--;
+    samples--;
   }
 
   // TODO: store the weights and biases and make it loadable to the model
