712de45c50bd677561669bd8398dcb8ad9969c30
jmaczan <jedrzejpawel@maczan.pl>
2025-05-24 20:32:28 +0200

    Forward pass

--- mlp.c
+++ mlp.c
@@ -1,13 +1,13 @@
+// Here's how learning emerges from C, loops and floats
+
+// MNIST
+// MLP 784 (28*28 black/white pixels) -> 128 (hidden layer) -> 10 (digits)
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-// Here's how learning emerges from C, loops and floats
-
-// MNIST
-// MLP 784 (28*28 black/white pixels) -> 128 (hidden layer) -> 10 (digits)
 
 #define LINE_BUF_SIZE 3137 // 1+784 * 4
 #define INPUT_DIM 784      // 28 x 28
@@ -25,6 +25,7 @@ void skip_csv_headers(FILE *f) {
     ;
 }
 
+// Random double number from range [0;1]
 float get_random_number() { return (double)rand() / (double)RAND_MAX; }
 
 // He initialization
@@ -36,21 +37,13 @@ void initialize_weights(float *weights, int weights_length, int layer_size) {
   }
 }
 
-// memory:
-// stack (up to 8MB)
-// [] - [] - [skip_csv_headers] - [main]
-
-// heap
-//
-
 int main() {
-  char line[LINE_BUF_SIZE]; // on Stack
+  char line[LINE_BUF_SIZE];
   FILE *f = fopen("data/mnist_train.csv", "r");
 
   skip_csv_headers(f);
 
-  float *weights_1 =
-      malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float)); // on Heap
+  float *weights_1 = malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float));
   float *weights_2 = malloc(HIDDEN_LAYER_DIM * OUTPUT_DIM * sizeof(float));
 
   float *biases_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
@@ -60,6 +53,9 @@ int main() {
   initialize_weights(weights_2, HIDDEN_LAYER_DIM * OUTPUT_DIM,
                      HIDDEN_LAYER_DIM);
 
+  float *activations_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
+  float *activations_2 = calloc(OUTPUT_DIM, sizeof(float));
+
   int samples = MAX_TRAINING_EXAMPLES;
   while (samples > 0) {
     fgets(line, LINE_BUF_SIZE, f);
@@ -68,19 +64,51 @@ int main() {
     char label = token[0];
     float inputs[INPUT_DIM];
 
-    // printf("%c \n", label);
     int i = 0;
     while (i < INPUT_DIM) {
-      //   printf("%s \n", token);
       token = strtok(NULL, ",\n");
-      inputs[i] = atoi(token) / 255.0f; // a room for performance improvements
-                                        //   printf("%f \n", input[i]);
+      inputs[i] = atoi(token) / 255.0f;
       i++;
     }
 
-    // TODO: forward pass
+    for (int i = 0; i < INPUT_DIM; i++) {
+      for (int j = 0; j < HIDDEN_LAYER_DIM; j++) {
+        activations_1[j] += weights_1[i * HIDDEN_LAYER_DIM + j];
+      }
+    }
 
-    // TODO: store intermediate values - pre-activations and activations
+    for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
+      activations_1[i] += biases_1[i];
+      activations_1[i] =
+          RELU(activations_1[i]); // should I store pre-activations and
+                                  // activations separately for backprop?
+    }
+
+    for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
+      for (int j = 0; j < OUTPUT_DIM; j++) {
+        activations_2[j] += weights_2[i * HIDDEN_LAYER_DIM + j];
+      }
+    }
+
+    for (int i = 0; i < OUTPUT_DIM; i++) {
+      activations_2[i] += biases_2[i];
+    }
+
+    float denominator = 0;
+
+    float exponents[OUTPUT_DIM];
+
+    for (int i = 0; i < OUTPUT_DIM; i++) {
+      exponents[i] = exp(activations_2[i]);
+      denominator += exponents[i];
+    }
+
+    float outputs[OUTPUT_DIM];
+    for (int i = 0; i < OUTPUT_DIM; i++) {
+      outputs[i] = exponents[i] / denominator;
+    }
+
+    // softmax here I guess
 
     // TODO: backward pass
 
