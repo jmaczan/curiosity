6601ff2d1bbdab4b5a9478914ebc7e322d18cbd1
jmaczan <jedrzejpawel@maczan.pl>
2025-05-24 22:39:21 +0200

    Load model params based on execution args - in progress

--- mlp.c
+++ mlp.c
@@ -37,24 +37,76 @@ void initialize_weights(float *weights, int weights_length, int layer_size) {
   }
 }
 
-int main() {
+void load_weights_and_biases(float *weights_1, int num_weights_1,
+                             float *weights_2, int num_weights_2,
+                             float *biases_1, int num_biases_1, float *biases_2,
+                             int num_biases_2) {
+  char line[2000000]; // 1177084
+  FILE *f = fopen("data/model_weights.csv", "r");
+
+  fgets(line, LINE_BUF_SIZE, f);
+  char *parameters = strtok(line, ",\n"); // TODO: why ",\n" instead of "\n"?
+
+  parameters = strtok(NULL, ",\n");
+  printf("%c \n", parameters[4]);
+  for (int i = 0; i < num_weights_1; i++) {
+    weights_1[i] = atoi(parameters);
+  }
+
+  printf("%f \n", weights_1[0]);
+}
+
+void forward(float *weights_1, float *weights_2, float *biases_1,
+             float *biases_2, float *activations_1, float *activations_2) {
+  for (int i = 0; i < INPUT_DIM; i++) {
+    for (int j = 0; j < HIDDEN_LAYER_DIM; j++) {
+      activations_1[j] += weights_1[i * HIDDEN_LAYER_DIM + j];
+    }
+  }
+
+  for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
+    activations_1[i] += biases_1[i];
+    activations_1[i] =
+        RELU(activations_1[i]); // should I store pre-activations and
+                                // activations separately for backprop?
+  }
+
+  for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
+    for (int j = 0; j < OUTPUT_DIM; j++) {
+      activations_2[j] += weights_2[i * HIDDEN_LAYER_DIM + j];
+    }
+  }
+
+  for (int i = 0; i < OUTPUT_DIM; i++) {
+    activations_2[i] += biases_2[i];
+  }
+
+  // softmax
+  float denominator = 0;
+
+  float exponents[OUTPUT_DIM];
+
+  for (int i = 0; i < OUTPUT_DIM; i++) {
+    exponents[i] = exp(activations_2[i]);
+    denominator += exponents[i];
+  }
+
+  float outputs[OUTPUT_DIM];
+  for (int i = 0; i < OUTPUT_DIM; i++) {
+    outputs[i] = exponents[i] / denominator;
+  }
+}
+
+void train(float *weights_1, float *weights_2, float *biases_1, float *biases_2,
+           float *activations_1, float *activations_2) {
   char line[LINE_BUF_SIZE];
   FILE *f = fopen("data/mnist_train.csv", "r");
 
   skip_csv_headers(f);
 
-  float *weights_1 = malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float));
-  float *weights_2 = malloc(HIDDEN_LAYER_DIM * OUTPUT_DIM * sizeof(float));
-
-  float *biases_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
-  float *biases_2 = calloc(OUTPUT_DIM, sizeof(float));
-
   initialize_weights(weights_1, INPUT_DIM * HIDDEN_LAYER_DIM, INPUT_DIM);
   initialize_weights(weights_2, HIDDEN_LAYER_DIM * OUTPUT_DIM,
                      HIDDEN_LAYER_DIM);
-
-  float *activations_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
-  float *activations_2 = calloc(OUTPUT_DIM, sizeof(float));
 
   int samples = MAX_TRAINING_EXAMPLES;
   while (samples > 0) {
@@ -67,48 +119,13 @@ int main() {
     int i = 0;
     while (i < INPUT_DIM) {
       token = strtok(NULL, ",\n");
-      inputs[i] = atoi(token) / 255.0f;
+      inputs[i] = atoi(token) / 255.0f; // there might be a bug here, it's
+                                        // possible I load label as first input
       i++;
     }
 
-    for (int i = 0; i < INPUT_DIM; i++) {
-      for (int j = 0; j < HIDDEN_LAYER_DIM; j++) {
-        activations_1[j] += weights_1[i * HIDDEN_LAYER_DIM + j];
-      }
-    }
-
-    for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
-      activations_1[i] += biases_1[i];
-      activations_1[i] =
-          RELU(activations_1[i]); // should I store pre-activations and
-                                  // activations separately for backprop?
-    }
-
-    for (int i = 0; i < HIDDEN_LAYER_DIM; i++) {
-      for (int j = 0; j < OUTPUT_DIM; j++) {
-        activations_2[j] += weights_2[i * HIDDEN_LAYER_DIM + j];
-      }
-    }
-
-    for (int i = 0; i < OUTPUT_DIM; i++) {
-      activations_2[i] += biases_2[i];
-    }
-
-
-    // softmax
-    float denominator = 0;
-
-    float exponents[OUTPUT_DIM];
-
-    for (int i = 0; i < OUTPUT_DIM; i++) {
-      exponents[i] = exp(activations_2[i]);
-      denominator += exponents[i];
-    }
-
-    float outputs[OUTPUT_DIM];
-    for (int i = 0; i < OUTPUT_DIM; i++) {
-      outputs[i] = exponents[i] / denominator;
-    }
+    forward(weights_1, weights_2, biases_1, biases_2, activations_1,
+            activations_2); // TODO get outputs
 
     // TODO: backward pass
 
@@ -117,8 +134,40 @@ int main() {
     // printf("%s", line);
     samples--;
   }
+}
 
-  // TODO: store the weights and biases and make it loadable to the model
+int main(int argc, char *argv[]) {
+  float *weights_1 = malloc(INPUT_DIM * HIDDEN_LAYER_DIM * sizeof(float));
+  float *weights_2 = malloc(HIDDEN_LAYER_DIM * OUTPUT_DIM * sizeof(float));
+
+  float *biases_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
+  float *biases_2 = calloc(OUTPUT_DIM, sizeof(float));
+
+  float *activations_1 = calloc(HIDDEN_LAYER_DIM, sizeof(float));
+  float *activations_2 = calloc(OUTPUT_DIM, sizeof(float));
+
+  if (argc == 2) {
+    if (strcmp(argv[1], "train") == 0) {
+      train(weights_1, weights_2, biases_1, biases_2, activations_1,
+            activations_2);
+    }
+    if (strcmp(argv[1], "run") == 0) {
+      load_weights_and_biases(weights_1, INPUT_DIM * HIDDEN_LAYER_DIM,
+                              weights_2, HIDDEN_LAYER_DIM * OUTPUT_DIM,
+                              biases_1, HIDDEN_LAYER_DIM, biases_2, OUTPUT_DIM);
+      forward(weights_1, weights_2, biases_1, biases_2, activations_1,
+              activations_2);
+    }
+  }
+
+  free(weights_1);
+  free(weights_2);
+  free(biases_1);
+  free(biases_2);
+  free(activations_1);
+  free(activations_2);
+
+  return 0;
 }
 
 // much later TODO:
