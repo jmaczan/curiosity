f9a25c10ff06d0ce93fe580456940ffc2ac1d618
Jędrzej Maczan <jedrzejpawel@maczan.pl>
2025-06-07 21:00:26 +0200

    Update val.sh

--- paged_out/val.sh
+++ paged_out/val.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -O3 -o mlp https://raw.githubusercontent.com/jmaczan/curiosity/refs/heads/main/mlp.c -lm && curl -s https://raw.githubusercontent.com/jmaczan/curiosity/refs/heads/main/paged_out/floats.txt | while IFS= read -r line; do
+gcc -O3 -o mlp https://raw.githubusercontent.com/jmaczan/curiosity/refs/heads/main/paged_out/pagedout_mlp.c -lm && curl -s https://raw.githubusercontent.com/jmaczan/curiosity/refs/heads/main/paged_out/floats.txt | while IFS= read -r line; do
     label=${line%%,*}
     rest=${line#*,}
     echo -n "Label: $label, "
